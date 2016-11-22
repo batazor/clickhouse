85d2a53d0eac23e1ec7507732aab3de57226b619
Login Viktor <batazor111@gmail.com>
2016-11-22 03:16:43 +0300

    Update Travis CI

--- docker-hub.sh
+++
@@ -1,3 +0,0 @@
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t batazor/clickhouse-server:$TRAVIS_TAG .
-docker push batazor/clickhouse-server:$TRAVIS_TAG
